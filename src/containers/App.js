0041ecdada9a4100d8b9b3c6a1c17e3402e4affc
Silvia Chung <schung97@gmail.com>
2018-01-16 14:04:27 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import {  BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
@@ -13,7 +10,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+       <NavBar />
+       <Route exact path="/" render={Home} />
+       <Route exact path="/movies" render={Movies} />
+       <Route exact path="/directors" render={Directors} />
+       <Route exact path="/actors" render={Actors} />
+     </div>
     </Router>
   );
 };
