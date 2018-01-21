f87c0a5691f4d8ba0f424c0839682e46b6e5c967
c0nniewang <connie.wang303@gmail.com>
2018-01-21 16:17:05 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  console.log(movies)
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie => 
+          <div>
+            {movie.title}, {movie.time}
+            {movie.genres.map(genre => <ul>{genre}</ul>)}
+          </div>)}
     </div>
   );
 };
